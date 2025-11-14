591cf522be21c98bbc302ae5dfad4d47acb1df54
mahi rudakiya <MR1698@live.mdx.ac.uk>
2025-11-14 21:46:28 +0530

    register, hash and login files

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print("hello")
+from log_hash import register_user, log_in_user
+
+def menu():
+   print('*'* 30)
+   print('*** welcome to my system ***')
+   print('choose from the following options:')
+   print('1. register')
+   print('2. log in')
+   print('3. exit')
+   print('*'* 30)
+   
+   
+def main():
+   while True:
+      menu()
+      choice = input('> ')
+      if choice == '1':
+         register_user()
+      elif choice == '2':
+         if log_in_user():
+            print('you loged in !!')
+         else:
+            print('incorect log in')
+      elif choice == '3':
+        print('good bye!!')
+      break
+   
+
+if __name__ == '__main__':
+   main()
+
+         
+
+
+
+
+
+    
